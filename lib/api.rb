fcf42545d919dc6f349e4d32307d470d37542920
FletchyAF <joshua.f1998@gmail.com>
2021-02-06 15:40:50 -0500

    setting up Api.rb class

--- lib/api.rb
+++ lib/api.rb
@@ -1,4 +1,21 @@
-class API
+require 'net/http'
+require 'open-uri'
+require 'json'
 
+
+
+class Api
+   
+      URL = "https://api.openbrewerydb.org/breweries"
+    
+      def get_breweries
+        uri = URI.parse(URL)
+        response = Net::HTTP.get_response(uri)
+        response.body
+      end
+    
+    
+    breweries = Api.new.get_breweries
+    puts breweries
 
 end
